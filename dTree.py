6ec6f1848321dcce32e4aa5f284290f1e9b1f890
shahriar1489 <shahriar294@gmail.com>
2024-03-09 15:32:10 -0700

    id3 implentation- tree makes recursive calls as required : IN PROGRESS

--- dTree.py
+++ dTree.py
@@ -1,77 +1,46 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 18 12:11:08 2024
-
-@author: shahriar
-"""
-
 import numpy as np 
 import pandas as pd
-import matplotlib.pyplot as plt 
-
-
-import os
-import time 
-
-
-import sys
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 # Arithmetic 
 from fractions import Fraction
 from decimal import Decimal
 
+class Node:
+    def __init__(self, attribute=None, value=None):
+        self.attribute = attribute
+        self.value = value
+        self.children = {}
 
-
-"""
-
-- Along the pipeline, keep the dataframe as pandas for as long as possible. 
-
-- 
-
-- 
-
-- 
-
-"""
-
+    def add_child(self, value, node):
+        self.children[value] = node
+        
+class Leaf:
+    def __init__(self, value=None):
+        self.value = value
 
 class ID3: 
-    
-    def __init__(self, df, attributes):
-        
-        """
-        
-        Parameters
-        ----------
-        examples : training examples
-        
-        target_attribute : attriburte whose value is to be predicted by tree 
-            
-        attributes : list of other attributes to be tested 
-
-        Returns
-        -------
-        Returns a decision tree that correctly classsifies the examples
-
-        """
-        
-        
-        
-        
-        examples = df[:, 0:-1]
-        target_attribute = df[:, -1]
+    def __init__(self, t_depth):
+        self.t_depth = t_depth
         
         # 1. create  a root for the tree
-        root = [] 
+        self.value = None
+        
+    def dTree(self, df, attributes):
+        
+        examples = df.iloc[:, 0:-1]
+        target_attribute = df.iloc[:, -1]
+        root = None
+        
         
         # 2 and 3. All examples have same label -> return single node with that label
         target_values , target_counts = np.unique(target_attribute, return_counts=True)
         
-        if len(target_values) ==1: 
-            
-            return root, target_values[0] # I am not sure how this will look  
         
+        if len(target_values) ==1:
+            return Leaf(target_values[0])
+            #return {'root': self.root, 'tvalue': target_values[0]} # I am not sure how this will look  
         
         
         # 4.attributes is empty 
@@ -82,9 +51,8 @@ class ID3:
             # Find the most frequent target value
             target_value, target_count = np.unique(target_attribute, return_counts=True)
             most_frequent_value = target_value[np.argmax(target_count)]
-            
-            
-            return root, most_frequent_value
+            return Leaf(most_frequent_value)
+            #return {'root': self.root, 'tvalue': most_frequent_value}
         
         # 5. Do this - this is where is information gain is calculated 
         """
@@ -98,7 +66,7 @@ class ID3:
         
         A = None
         
-        highest_info_gain = -100000.0
+        highest_info_gain = -np.inf
         
         for a in attributes: # find the attribute with the highest information gain 
             
@@ -107,7 +75,7 @@ class ID3:
             For now, assume all are discrete. 
             """
             
-            info_gain = self.information_gain( examples[a], target_attribute, 'entropy') # examples is pd df
+            info_gain = self.information_gain_for_discrete_attribute( examples[a], target_attribute ) # examples is pd df
                     
             
             if info_gain > highest_info_gain: 
@@ -115,11 +83,17 @@ class ID3:
                 highest_info_gain = info_gain 
         
         # set root to A 
-        root.append(A) 
+        if root == None:
+            root = Node(attribute=A)
+        
+        #else:
+            #self.root.add_child(self.value, Node(attribute=A))
+            #print('self.root.add_child:',self.root.children[self.value])
+            #self.value = None
 
         # get the unique values in A
         vi_list_np = np.unique(examples[A]) # examples is pandas df
-        vi_with_root = [] 
+        #vi_with_root = [] 
         
 
         vi_count = 0 
@@ -127,11 +101,13 @@ class ID3:
         
         for vi in vi_list_np : 
 
-            vi_count = vi_count+1 
+            #vi_count = vi_count+1 
             
             # Add a new branch below root, corresponding to the test A = v_i 
-            vi_with_root.append(A+'->'+vi) # a list 
-            root.append( vi_with_root )
+            #vi_with_root.append(A+'->'+vi) # a list 
+            #self.root = Node(attribute=A, value=vi)
+            #print('self.root:', self.root.children)
+            #self.value = vi
             
             # Let Examples_vi be the subest of examples that have value v_i for A 
             examples_vi  = examples[ examples[A] == vi ]
@@ -149,68 +125,18 @@ class ID3:
                 # Then below this new branch add a leaf node with label = most common value of Target_attribute in Examples
                 target_value, target_count = np.unique(target_attribute, return_counts=True)
                 most_frequent_value = target_value[np.argmax(target_count)]
-                
-                return root, most_frequent_value
+                return Leaf(most_frequent_value)
                 
                 
             else: #  
-                ID3(df[ df[ A ] == vi], attributes.remove(A))
+                if A in attributes:
+                    attributes.remove(A)
+                root.add_child(vi, self.dTree(df[ df[ A ] == vi], attributes)) 
                 
             
-            vi_with_root = [] 
-            
-            
-            """
-            I am thinking of using dictionary as for the mean branching an attribute
-            
-            - Root is a list
-            
-            """
-            
-        #A = None 
-        #examples_vi = [] #None 
-        
-        #branches = [] # the branches of the tree
-        
-        
-        #if len( branches_vi) == 0: 
-            # Then below this new branch add a  leaf node with label = most common value of Target_attribute in Examples
-        
-        
-        #else:  # below this new branch add the subtree 
-        #    ID3( examples_vi,  target_attribute, )
-        
-        
-        
-    def information_gain( examples, target_attribute, attribute, impurity='gini'): 
-        """
-
-        Parameters
-        ----------
-        examples : numpy array 
-            the feature columns whose information gain is to be calculated 
-            
-        target_attribute : numpy array 
-            the target label - y
-            
-        attribute : string 
-            the attribute whose info gain to be calculated 
-        
-        impurity : string 
-            the impurity measure - gini or entropy 
-        
-
-        Returns
-        -------
-        numpy scalar real number 
-
-        """
-        return -1 
-        
-            
-    
-    
-      
+            #vi_with_root = []
+        return root
+     
         
     def compute_impurity_by_label(self, attribute, impurity='gini'): # Impurity of the total dataset : DONE
         
@@ -232,7 +158,7 @@ class ID3:
         # get the total number of instances/rows in the dataset
         N = attribute.shape[0]
         
-        print('\t\t Number of rows in attribute param:', N)
+        #print('\t\t Number of rows in attribute param:', N)
         #sys.exit(0)
     
         # get the count
@@ -243,28 +169,28 @@ class ID3:
         # get the fractions for the each of the labels- better to use loop be cause there can be more than two labels
     
         for count in label_counts :
-            print(Decimal(count/N)) 
+            #print(Decimal(count/N)) 
             
             result_float = float( count/ Decimal(N) )
             
             label_fractions.append( result_float  )
     
     
-        print('\t\tlabel_fractions: ',label_fractions)
+        #print('\t\tlabel_fractions: ',label_fractions)
         
         label_fractions = np.array( label_fractions )
-        print('\t\tDifferent label values collected: ', label_values)
-        print('\t\tDifferent label counts colleceted: ', label_counts)
-        print('\t\tFractions of different labels: ', label_fractions)
+        #print('\t\tDifferent label values collected: ', label_values)
+        #print('\t\tDifferent label counts colleceted: ', label_counts)
+        #print('\t\tFractions of different labels: ', label_fractions)
     
     
         # write a subroutine for entropy
         if impurity=='entropy':
             #return  - np.sum ( label_fractions * np.log2(  label_fractions ) ) # This returns the complete entropy 
-            print('-------------\n\n\n')
+            #print('-------------\n\n\n')
             #print("\t\t\tInside impurity=entropy",  -1 * label_fractions * np.log2(label_fractions) ) 
     
-            print("-------------\t\t\tnp.sum = ", -np.sum(  label_fractions * np.log2(label_fractions) ) )
+            #print("-------------\t\t\tnp.sum = ", -np.sum(  label_fractions * np.log2(label_fractions) ) )
             
             
             return -np.sum(  label_fractions * np.log2(label_fractions) )
@@ -320,9 +246,9 @@ class ID3:
         
         N = examples_a.shape[0]
         
-        result = self.compute_impurity_by_label(  target_attribute=target_attribute , impurity=impurity)
+        result = self.compute_impurity_by_label(  attribute=target_attribute , impurity=impurity)
         
-        print( '\t\t\tresult after initialization : ', result) # ok 
+        #print( '\t\t\tresult after initialization : ', result) # ok 
         
         #sys.exit(0)
         for a in examples_a_values: 
@@ -356,13 +282,93 @@ class ID3:
             
             result = result - impurity_a
             
-            print('\t\t---------------\t\t\n')
+            #print('\t\t---------------\t\t\n')
             
             
             
-            print('\t\t\t--- final info gain : ', result )
+            #print('\t\t\t--- final info gain : ', result )
             
-        return result # returns a scalar real number         
+        return result # returns a scalar real number 
+    
+    def predict(self, X, tree):
+        return np.array([self.predictTree(x[1], tree) for x in X.iterrows()])
+        
+    def predictTree(self, x, tree):
+        
+        while not isinstance(tree, Leaf):
+            # Get the attribute value in x
+            attribute_value = x[tree.attribute]
+            
+        
+            # Check if the attribute value exists in the children of the current node
+            if attribute_value in tree.children:
+            #    # Move to the child node corresponding to the attribute value
+                tree = tree.children[attribute_value]
+
+    
+        # Return the value of the leaf node as the predicted class label
+        return tree.value
+    
+class RandomForest:
+    
+    def __init__(self, n_trees) -> None:
+        self.Forest = []
+        self.n_trees = n_trees
+        
+    def treesinRF(self, df, attributes):
+        for _ in self.n_trees:
+            tree = ID3(30)
+            df_bs = self.bootStrap(df)
+            tree.dTree(df_bs, attributes)
+            self.Forest.append(tree)
+            
+    def bootStrap(self, df):
+        ind = np.random.choice(df.shape[0], size= df.shape[0], replace= True)
+        return df[ind]
+    
+    def predict(self, X):
+        tree_pred = np.swapaxes(np.array([tree.predict(X, tree) for tree in self.Forest]), 0, 1)
+        forest_predictions = np.array([np.bincount(pred).argmax() for pred in tree_pred])
+        return forest_predictions     
+    
+df = pd.read_csv('/Users/rahulpayeli/Documents/ML/tease.csv')
+df.drop(columns='TransactionID' , inplace=True)
+att = df.columns.tolist()
+att.remove('isFraud')
+
+#X = df.drop(columns=['isFraud']).values
+#y = df['isFraud'].values
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+X = test_df.iloc[:, 0:-1]
+Y = test_df.iloc[:, -1]
+#print('traindf', train_df)
+
+pred = ID3(30)
+rootNode = pred.dTree(train_df, att)
+#print('X:',Y)
+predictions = pred.predict(X, rootNode)
+
+def accuracy(y_test, y_pred):
+  return np.sum(y_test == y_pred) / len(y_test)
+
+acc = accuracy(Y, predictions)
+print("Accuracy: ", acc)
+#print('Tree:', pred.dTree(train_df, att).children['Sunny'].children['Normal'].value)
+#dfs_display(pred.dTree(train_df, att))
+#predictions = pred.predict(test_df, att)
+
+#def accuracy(y_test, y_pred):
+#  return np.sum(y_test == y_pred) / len(y_test)
+
+#acc = accuracy(test_df, predictions)
+#print("Accuracy: ", acc)
+    
+    
+    
+
+    
+    
     
     
 
